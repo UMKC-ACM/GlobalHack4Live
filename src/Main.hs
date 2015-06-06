9e6e52977069ac9e9ea158ce47875d29c59a3a90
Chris Denniston <octopuscabbage@gmail.com>
2015-06-06 02:07:58 -0500

    yeah

---
+++ src/Main.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+import Snap.Http.Server
+import Snap.Util.FileServe
+import qualified Snap.Core as Core
+
+fileHandle = Core.route [("",serveFile "www/index.html"),
+                        ("",serveDirectory "www")]
+
+handle = fileHandle
+
+main = quickHttpServe handle
