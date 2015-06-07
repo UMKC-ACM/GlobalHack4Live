e807eb4c2a6389ee864c4eb67b13a4178d01269d
Mike <ppnguyen91@gmail.com>
2015-06-07 07:45:57 -0500

    push

--- www/test.js
+++ www/test.js
@@ -1,7 +1,7 @@
 
 
   var annot = [{
-  "src":"http://127.0.0.1:8080/image/640px-Hallstatt.jpg",
+  "src":"http://kdmedia.kanald.ro/kfetele/image/201406/w650/media_140376880269804600.jpg",
   "text":"hey",
   "shapes":[{
         "type":"rect",
